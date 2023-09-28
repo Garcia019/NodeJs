947132b78402f75d179d5406fdbc87a0de2542fd
Garcia019 <matheugarcia2010@gmail.com>
2023-09-27 21:44:49 -0300

    Aprendendo sobre o gerenciado de pacotes NPM, boas práticas e importação de módulos.

---
+++ index.js
@@ -0,0 +1,6 @@
+var validator = require("validator");
+var calculador = require("./calculador.js")
+console.log(calculador.somar(1,2))
+console.log(calculador.subtrair(1,2))
+console.log(calculador.multiplicar(1,2))
+console.log(calculador.dividir(1,2))
