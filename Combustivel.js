33623f08af65659a8e5809a5972da95a316cef0d
Garcia019 <matheugarcia2010@gmail.com>
2023-10-01 14:54:34 -0300

    Atualização dos testes, em todas os possíveis resultados

--- Combustivel.js
+++ Combustivel.js
@@ -2,10 +2,15 @@ const calculadora = require("./calculadora.js")
 
 function calcularGasto(distancia, combustivel){
     let rendimento
+    if(distancia < 0){
+        return false
+    }
     if(combustivel.toUpperCase() === "ÁLCOOL"){
         rendimento = 11
     }else if(combustivel.toUpperCase() === "GASOLINA"){
         rendimento = 16
+    }else{
+        return false
     }
     return Math.ceil((calculadora.dividir(Math.ceil(distancia), rendimento)))
 }
--- test/calculadorGasto.test.js
+++ test/calculadorGasto.test.js
@@ -7,3 +7,15 @@ test("Testando calculo de combustível com álcool e 40 km.", () => {
 test("Testando calculo de combustível com gasolina e 40 km.", () => {
     expect(combustivel.calcularGasto(40, "gasolina")).toBe(3)
 });
+
+test("Testando calculo de combustível com gasolina e distancia negativa", () => {
+    expect(combustivel.calcularGasto(-40, "gasolina")).toBe(false)
+});
+
+test("Testando calculo de combustível com álcool e distancia negativa", () => {
+    expect(combustivel.calcularGasto(-40, "álcool")).toBe(false)
+});
+
+test("Testando calculo de combustível com nenhum combustével e 40km", () => {
+    expect(combustivel.calcularGasto(40,"")).toBe(false)
+});
